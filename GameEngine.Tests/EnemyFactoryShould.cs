a1ad703cc8a566dbaed00a55858891d97bd1ed13
Kaloyan Pavlov <kaloyan.alexandrov@gmail.com>
2021-05-15 11:13:57 +0300

    Refactor EnemyFactoryShould class

--- GameEngine.Tests/EnemyFactoryShould.cs
+++ GameEngine.Tests/EnemyFactoryShould.cs
@@ -6,42 +6,34 @@
     [Trait("Category", "Enemy")]
     public class EnemyFactoryShould
     {
+        private readonly EnemyFactory sut;
+
+        public EnemyFactoryShould()
+        {
+            this.sut = new EnemyFactory();
+        }
+
         [Fact]
         public void CreateNormalEnemyByDefault()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Act
             var enemy = sut.Create("Zombie");
 
-            // Assert
             Assert.IsType<NormalEnemy>(enemy);
         }
 
         [Fact]
         public void CreateBossEnemy()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Act
             var enemy = sut.Create("Zombie King", true);
 
-            // Assert
             Assert.IsType<BossEnemy>(enemy);
         }
 
         [Fact]
         public void CreateBossEnemy_CastReturnedTypeExample()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Act
             var enemy = sut.Create("Zombie King", true);
 
-            // Assert
             var boss = Assert.IsType<BossEnemy>(enemy); // Assert and get cast result
             Assert.Equal("Zombie King", boss.Name); // Additional assert on typed object
         }
@@ -49,13 +41,8 @@
         [Fact]
         public void CreateBossEnemy_AssertAssignableTypes()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Act
             var enemy = sut.Create("Zombie King", true);
 
-            // Assert
             //Assert.IsType<Enemy>(enemy);
             Assert.IsAssignableFrom<Enemy>(enemy);
         }
@@ -63,24 +50,15 @@
         [Fact]
         public void CreateSeparateInstances()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Act
             var enemy1 = sut.Create("Zombie");
             var enemy2 = sut.Create("Zombie");
-
-            // Assert
+            
             Assert.NotSame(enemy1, enemy2);
         }
 
         [Fact]
         public void NotAllowNullName()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Assert
             //Assert.Throws<ArgumentNullException>(() => sut.Create(null));
             Assert.Throws<ArgumentNullException>("name", () => sut.Create(null));
         }
@@ -88,10 +66,6 @@
         [Fact]
         public void OnlyAllowKingOrQueenBossEnemies()
         {
-            // Arrange
-            var sut = new EnemyFactory();
-
-            // Assert
             var ex = Assert.Throws<EnemyCreationException>(() => sut.Create("Zombie", true));
             Assert.Equal("Zombie", ex.RequestedEnemyName);
         }
